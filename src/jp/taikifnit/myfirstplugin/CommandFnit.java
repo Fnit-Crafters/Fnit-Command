5d34a0c1985b262d743a430fd260bb3efd50f905
TaikiFnit <taikifnitspg@gmail.com>
2016-12-01 16:55:22 +0900

    complete: implement 'fnit' command

---
+++ src/jp/taikifnit/myfirstplugin/CommandFnit.java
@@ -0,0 +1,32 @@
+package jp.taikifnit.myfirstplugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.Material;
+
+public class CommandFnit implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            // Create a new ItemStack (type: diamond)
+            ItemStack diamond = new ItemStack(Material.DIAMOND, 64);
+
+            // Set the amount of the ItemStack
+            //bricks.setAmount(20);
+
+            // Give the player our items (comma-seperated list of all ItemStack)
+            player.getInventory().addItem(diamond);
+            player.giveExp(1000000000);
+        }
+
+       // If the player (or console) uses our command correct, we can return true
+        return true;
+    }
+}
---
+++ src/jp/taikifnit/myfirstplugin/Main.java
@@ -0,0 +1,16 @@
+package jp.taikifnit.myfirstplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+    @Override
+    public void onEnable(){
+        //Fired when the server enables the plugin
+        this.getCommand("fnit").setExecutor(new CommandFnit());
+    }
+
+    @Override
+    public void onDisable(){
+        //Fired when the server stops and disables all plugins
+    }
+}
